ab7ae9f74c4d561c01ca0917d908afbdc93c0797
Marcos-A <marcosalcocer@gmail.com>
2022-06-25 06:08:31 +0200

    Improvements in 'text_to_music_folder_format.py'.

--- text_to_music_folder_format.py
+++ text_to_music_folder_format.py
@@ -13,24 +13,30 @@ import re
 # Grab the query with the entered text
 text_str = sys.argv[1]
 
-# Add spaces before and after dashes
-dash_without_spaces = re.search("[A-Za-zÀ-ÖØ-öø-ÿ0-9_,.\s]+[-][A-Za-zÀ-ÖØ-öø-ÿ0-9_,.()\s]", text_str)
-if dash_without_spaces:
-    text_str = text_str.replace("-", " - ")
 # Replace undescores with spaces
 text_str = text_str.replace("_", " ")
+
+# Add spaces before and after dashes separating artist and album
+dash_without_spaces = re.search("[A-Za-zÀ-ÖØ-öø-ÿ0-9_,.\s]+^(_)[-]^(_)[A-Za-zÀ-ÖØ-öø-ÿ0-9_,.()\s]", text_str)
+if dash_without_spaces:
+    text_str = text_str.replace("-", " - ")
+
 # Capitalize every word
 text_str = ' '.join([word.capitalize() for word in text_str.split(' ')])
+
 # Replace spaces with undescores
 text_str = text_str.replace(" ", "_")
+
 # Replace slashes with ampersands
 text_str = text_str.replace("/", "&")
+
 # Format the dash separating the artist and the album
 dash_without_spaces = re.search("A-Za-zÀ-ÖØ-öø-ÿ0-9]+-[A-Za-zÀ-ÖØ-öø-ÿ0-9]+", text_str)
 if dash_without_spaces:
     text_str = text_str.replace("-", " - ")
 else:
     text_str = text_str.replace("_-_", " - ")
+
 # Format the year of release
 release_year_without_parenthesis = re.search("[A-Za-zÀ-ÖØ-öø-ÿ0-9_,.]+\s[-]\s[A-Za-zÀ-ÖØ-öø-ÿ_,.]+[_][0-9]{4}$", text_str)
 if release_year_without_parenthesis:
